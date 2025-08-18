6a32b3002cbc8c0788a1ba4a7e69c671da71819e
SunomS <absunom@gmail.com>
2025-08-18 22:37:35 +0600

    hero text
    18 aug

--- script.js
+++ script.js
@@ -310,3 +310,73 @@ const updateDOM = (opponentChoice, choiceDataType) => {
 };
 
 buttons.forEach((button) => button.addEventListener('click', getOutcomeAndUpdateDOM));
+
+//Other animation shorts
+//hero- text 
+
+const slideTL = gsap.timeline();
+const maskTL = gsap.timeline();
+const mainTL = gsap.timeline({
+    repeat: -1,
+    onRepeat: () => {
+        gsap.set('#texttwo', { opacity: 0 });
+        gsap.set('#bar', { scaleY: 0.1 });
+        gsap.set('#textone h2', { opacity: 1 });
+    }
+});
+
+gsap.set('#texttwo', { opacity: 0 });
+gsap.set('#bar', { scaleY: 0.1 });
+
+slideTL
+    .to('#bar', 1, {
+        y: 5,
+        scaleY: 1,
+        ease: "back.out"
+    })
+    .to('#slidebar', 1.5, {
+        x: 280,
+        delay: 0.5,
+        ease: "back.inOut(0.8)"
+    })
+    .to('#slidebar', 1.5, {
+        x: 0,
+        delay: 0.5,
+        ease: "back.inOut(0.8)"
+    })
+    .to('#slidebar', 1.5, {
+        x: 280,
+        delay: 0.5,
+        ease: "back.inOut(0.8)"
+    })
+    .to('#bar', 1, {
+        y: 200,
+        scaleY: 0.1,
+        ease: "back.in"
+    });
+
+maskTL
+    .to('#textone', 1.5, {
+        ease: "back.inOut(0.8)",
+        "clip-path": "polygon(0 0, 91% 0, 81% 100%, 0% 100%)",
+        onComplete: () => {
+            gsap.set('#texttwo', { opacity: 1 });
+        }
+    })
+    .to('#textone', 1.5, {
+        delay: 0.5,
+        ease: "back.inOut(0.8)",
+        "clip-path": "polygon(0 0, 18% 0, 8% 100%, 0% 100%)",
+        onComplete: () => {
+            gsap.set('#textone h2', { opacity: 0 });
+        }
+    })
+    .to('#textone', 1.5, {
+        delay: 0.5,
+        ease: "back.inOut(0.8)",
+        "clip-path": "polygon(0 0, 91% 0, 81% 100%, 0% 100%)"
+    });
+
+mainTL
+    .add(slideTL)
+    .add(maskTL, 1.5);
